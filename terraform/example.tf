e01fbd9f1f82134cb2952bf5bff8e83de097e402
Jack Yoon <worker.yoon@gmail.com>
2018-12-19 20:06:33 +0900

    getting started - provision

--- terraform/example.tf
+++ terraform/example.tf
@@ -2,15 +2,17 @@ provider "aws" {
   region     = "ap-northeast-2"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "terraform-getting-started-guide.worker.yoon"
-  acl = "private"
-}
-
 resource "aws_instance" "example" {
   ami = "ami-f9934097"
   instance_type = "t2.micro"
-  depends_on = ["aws_s3_bucket.example"]
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+  provisioner "local-exec" {
+    when = "destroy"
+    command = "rm ip_address.txt"
+  }
 }
 
 resource "aws_eip" "ip" {
