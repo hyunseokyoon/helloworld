dba50c5df5e2beac3621ce2623b42d52ec064c34
Jack Yoon <worker.yoon@gmail.com>
2018-12-19 20:47:51 +0900

    remove module

--- terraform/example.tf
+++ terraform/example.tf
@@ -2,29 +2,23 @@ provider "aws" {
   region     = "ap-northeast-2"
 }
 
-#resource "aws_instance" "example" {
-#  ami = "ami-f9934097"
-#  instance_type = "t2.micro"
-#
-#  provisioner "local-exec" {
-#    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-#  }
-#  provisioner "local-exec" {
-#    when = "destroy"
-#    command = "rm ip_address.txt"
-#  }
-#}
-#
-#resource "aws_eip" "ip" {
-#  instance = "${aws_instance.example.id}"
-#}
-#
-#output "ip" {
-#  value = "${aws_eip.ip.public_ip}"
-#}
+resource "aws_instance" "example" {
+  ami = "ami-f9934097"
+  instance_type = "t2.micro"
 
-module "consul" {
-  source = "hashicorp/consul/aws"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+  provisioner "local-exec" {
+    when = "destroy"
+    command = "rm ip_address.txt"
+  }
+}
 
-  num_servers = "3"
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
+output "ip" {
+  value = "${aws_eip.ip.public_ip}"
 }
