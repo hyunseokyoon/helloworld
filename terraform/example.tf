b3d29cf6ee1356aa4d109e73aaa287b4756da939
Jack Yoon <worker.yoon@gmail.com>
2018-12-19 18:49:53 +0900

    getting-started dependencies

--- terraform/example.tf
+++ terraform/example.tf
@@ -2,7 +2,18 @@ provider "aws" {
   region     = "ap-northeast-2"
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "terraform-getting-started-guide.worker.yoon"
+  acl = "private"
+}
+
 resource "aws_instance" "example" {
   ami = "ami-f9934097"
   instance_type = "t2.micro"
+  depends_on = ["aws_s3_bucket.example"]
 }
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
